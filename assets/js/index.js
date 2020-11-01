0fd482c142cc7fc9fd70ce6ab82942f24c6fba7a
longdan-qilin <1724550614@qq.com>
2020-11-01 11:35:39 +0800

    19-渲染用户头像

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,11 +10,36 @@ function getuserinfo() {
         url: '/my/userinfo',
         // headers  请求头配置
         headers: {
-            Authorization : localStorage.getItem('token') || ''
+            Authorization: localStorage.getItem('token') || ''
         },
-        success: function (res) { 
-            console.log(res);
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 获取成功  调用 渲染页面 函数 renderuser（）
+            renderuser(res.data)
         }
     })
+}
+
+// 通过获取的数据 渲染头像页面
+function renderuser(user) {
+    // 获取用户的名称
+    let name = user.usernike || user.username
+    // 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 获取用户头像 （如果用户有头像显示头像，否则显示用户的名称第一字大写 文字头像）
+    if (user.user_pic !== null) {
+        // 显示用户头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        // 文字头像隐藏
+        $('.tetx-avatar').hide()
+    } else {
+        // 隐藏用户头像
+        $('.layui-nav-img').hide()
+        // 文字头像显示
+        let first = name[0].toUppercase
+        $('.tetx-avatar').html(first).show()
+    }
 }
 
