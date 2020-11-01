5441410edacecafd79ad13724173bb3c12ed4778
longdan-qilin <1724550614@qq.com>
2020-11-01 14:31:00 +0800

    22-控制未登录用户的页面访问权限

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,6 +35,15 @@ function getuserinfo() {
             }
             // 获取成功  调用 渲染页面 函数 renderuser（）
             renderuser(res.data)
+        },
+        complete: function (res) {
+            // 成功 or 失败都会执行这里
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                // 1. 清空 token
+                localStorage.removeItem('token');
+                // 2. 强制跳转到登录页
+                location.href = '/login.html';
+            }
         }
     })
 }
