48a82d16410359342304a2221eaab25cf293ea4e
longdan-qilin <1724550614@qq.com>
2020-11-02 14:22:58 +0800

    39-在弹出框中渲染添加类别的表单结构

--- assets/js/article/art-cate.js
+++ assets/js/article/art-cate.js
@@ -8,7 +8,7 @@ $(function () {
             type: 1,
             area: ['500px', '250px'],
             title: '添加文章分类',
-            content:""
+            content: $('#dialog-add').html()
         })
     })
 })
