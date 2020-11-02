ecca6f4a072222aa64ddfbc11be2184864e6112b
longdan-qilin <1724550614@qq.com>
2020-11-02 11:44:55 +0800

    38-使用layer.open实现弹出层

--- assets/js/article/art-cate.js
+++ assets/js/article/art-cate.js
@@ -1,6 +1,16 @@
 $(function () {
     // 获取文字分类的列表
     initArtCateList()
+
+    // 添加类别弹框
+    $('#btnAddCate').on('click', function () {
+        layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content:""
+        })
+    })
 })
 
 function initArtCateList() {
@@ -13,3 +23,4 @@ function initArtCateList() {
         }
     })
 }
+
