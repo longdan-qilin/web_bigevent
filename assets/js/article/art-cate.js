5c31dddf34efa0867b623eba52e40d8fa4493e26
longdan-qilin <1724550614@qq.com>
2020-11-02 15:50:52 +0800

    42-为修改分类的弹出层填充当前数据

--- assets/js/article/art-cate.js
+++ assets/js/article/art-cate.js
@@ -1,64 +1,73 @@
 $(function () {
-    // 获取文字分类的列表
-    initArtCateList()
+    var layer = layui.layer;
+    var form = layui.form;
+    // 获取文章分类的列表
+    initArtCateList();
+
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                var htmlStr = template('tpl-table', res);
+                $('tbody').html(htmlStr);
+            }
+        });
+    }
 
     // 添加类别弹框
+    var indexAdd = null;
     $('#btnAddCate').on('click', function () {
-        layer.open({
+        indexAdd = layer.open({
             type: 1,
             area: ['500px', '250px'],
             title: '添加文章分类',
             content: $('#dialog-add').html()
-        })
-    })
-})
+        });
+    });
 
-// 添加类别弹框
-var indexAdd = null;
-function initArtCateList() {
-    $.ajax({
-        method: 'GET',
-        url: '/my/article/cates',
-        success: function (res) {
-            let hmtlStr = template('tpl-table', res)
-            $('tbody').html(hmtlStr)
-        }
-    })
-}
-
-// 添加文章分类，通过委托的形式为表单绑定提交事件
-$('body').on('submit', '#form-add', function (e) {
-    // 阻止表单默认提交
-    e.preventDefault()
-    // console.log(ok);
-    $.ajax({
-        method: 'POST',
-        url: '/my/article/addcates',
-        data: $(this).serialize(),
-        success: function (res) {
-            if (res.status !== 0) {
-                layui.layer.msg('新增文章分类失败！')
+    // 添加文章分类，通过委托的形式为表单绑定提交事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！');
+                }
+                // 重新获取分类数据
+                initArtCateList();
+                // 提示成功消息
+                layer.msg('新增分类成功！');
+                // 关闭弹出层
+                layer.close(indexAdd);
             }
-            // 重新获取分类数据
-            initArtCateList();
-            layui.layer.msg('新增文章分类成功！')
-            // 关闭弹出层
-            layui.layer.close(indexAdd)
-        }
-    })
-})
+        });
+    });
 
-// 通过代理的方式，为 btn-edit 按钮绑定点击事件
-var indexEdit = null;
-$('tbody').on('click', '.btn-edit', function (e) {
-    // 编辑弹出层
-     indexEdit = layer.open({
-        type: 1,
-        area: ['500px', '250px'],
-        title: '添加文章分类',
-        content: $('#dialog-edit').html()
-    })
-})
+    // 通过代理的方式，为 btn-edit 按钮绑定点击事件
+    var indexEdit = null;
+    $('tbody').on('click', '.btn-edit', function () {
+        // 修改分类信息的层
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        });
 
+        // 获取当前分类数据
+        var id = $(this).attr('data-id');
+        $.ajax({
+            method: 'GET',
+            url: `/my/article/cates/${id}`,
+            success: function (res) {
+                form.val('form-edit', res.data);
+            }
+        });
+    });
+});
 
 
