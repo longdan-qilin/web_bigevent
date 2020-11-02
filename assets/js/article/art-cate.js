29a48c19c6e661a3ad36486b198d2cb6c55200d1
longdan-qilin <1724550614@qq.com>
2020-11-02 14:51:12 +0800

    40-添加文章分类功能

--- assets/js/article/art-cate.js
+++ assets/js/article/art-cate.js
@@ -13,6 +13,8 @@ $(function () {
     })
 })
 
+// 添加类别弹框
+var indexAdd = null;
 function initArtCateList() {
     $.ajax({
         method: 'GET',
@@ -23,4 +25,28 @@ function initArtCateList() {
         }
     })
 }
+
+// 添加文章分类，通过委托的形式为表单绑定提交事件
+$('body').on('submit', '#form-add', function (e) {
+    // 阻止表单默认提交
+    e.preventDefault()
+    // console.log(ok);
+    $.ajax({
+        method: 'POST',
+        url: '/my/article/addcates',
+        data: $(this).serialize(),
+        success: function (res) {
+            if (res.status !== 0) {
+                layui.layer.msg('新增文章分类失败！')
+            }
+            // 重新获取分类数据
+            initArtCateList();
+            layui.layer.msg('新增文章分类成功！')
+            // 关闭弹出层
+            layui.layer.close(indexAdd)
+        }
+    })
+})
+
+
 
